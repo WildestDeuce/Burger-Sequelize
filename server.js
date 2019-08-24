77cd4b9765c95b93ce7963b450ae3fd660ee5e0d
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-08-24 18:52:27 -0400

    server.js imports mysql and express

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 let express = require("express");
+let mysql = require("mysql");
+
+let app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("app/public"))
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+var friendsArray = require("./app/data/friends"); 
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
